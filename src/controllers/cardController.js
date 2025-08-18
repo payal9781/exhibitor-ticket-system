e5c6778ba1cb7466f0b4feb010a39d592bf7065c
naidunandu <naidunandu237@gmail.com>
2025-08-18 15:45:54 +0530

    Card Controller

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const { successResponse, errorResponse } = require('../utils/apiResponse');
 const { models } = require('./../models/z-index');
+const axios = require('axios');
 
 exports.getCards = asyncHandler(async (req, res) => {
     let cards = await models.ScannedCards.find({ userId: req.user.id });
@@ -19,4 +20,39 @@ exports.deleteCard = asyncHandler(async (req, res) => {
     let { id } = req.body;
     await models.ScannedCards.findByIdAndDelete(id);
     return successResponse(res, true, 200);
+});
+
+exports.createDigitalCard = asyncHandler(async (req, res) => {
+    let { name, emailId, mobileNumber } = req.body;
+
+    //Save Card "Digital Card" Under Super Admin
+    const payload = {
+        name: String(name).trim(),
+        email: emailId || "",
+        mobile: mobileNumber,
+        businessKeyword: "--",
+        originId: "67ca6934c15747af04fff36c",
+        countryCode: "91"
+    };
+    var result = await axios.post(`https://gbscard.itfuturz.in/web/create-account/mobile',`, payload, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (result.data) {
+        let isVisitor = await models.Visitor.findById(req.user.id);
+        if (isVisitor) {
+            await models.Visitor.findByIdAndUpdate(req.user.id, { digitalProfile: result.data.path }, { new: true });
+            return successResponse(res, true, 200);
+        }
+        let isExibhitor = await models.Exhibitor.findById(req.user.id);
+        if (isExibhitor) {
+            await models.Exhibitor.findByIdAndUpdate(req.user.id, { digitalProfile: result.data.path }, { new: true });
+            return successResponse(res, true, 200);
+        }
+        return errorResponse(res, "Unable to update profile!", 500);
+    } else {
+        return errorResponse(res, "Failed to create digital card!", 500);
+    }
 });
