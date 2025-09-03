18e1ce18e9870af72504b419768e3fcd13687e21
Hitesh Suthar <hitesh@itfuturz.com>
2025-09-03 16:09:23 +0530

    working fine

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase-admin');
 
-var notificationAcc = require('../../testing-4b090-firebase-adminsdk-fbsvc-ed05361c31.json').notification;
+var notificationAcc = require('../../testing-4b090-firebase-adminsdk-fbsvc-ec4854046e.json').notification;
 
 const notification = () => {
   return firebase.initializeApp(
