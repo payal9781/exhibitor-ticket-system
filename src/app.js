9ff79de90c72663e33c402aaf18e5e029e771e62
rakeshitfuturz <rakesh@itfuturz.com>
2025-09-15 11:22:14 +0530

    error in live

--- src/app.js
+++ src/app.js
@@ -17,9 +17,8 @@ app.set('etag', false);
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 // Morgan logging configuration
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
